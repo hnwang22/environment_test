da5003d4c4380bdd15300a2a073a6b873e22001e
root <root@localhost>
2021-09-30 04:33:37 +0000

    unit/integration test 9-29

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,6 +1,6 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
-
+#integration test for book title attribute
 RSpec.describe 'Creating a book', type: :feature do
 	scenario 'valid inputs' do
 		visit new_book_path
@@ -10,3 +10,42 @@ RSpec.describe 'Creating a book', type: :feature do
 		expect(page).to have_content('harry potter')
 	end
 end
+
+#=begin
+#integration test for author attribute (string)
+RSpec.describe 'Book with author', type: :feature do
+	scenario 'valid inuputs' do
+		visit new_book_path
+		fill_in 'Author', with: 'Isaac Asimov'
+		click_on 'Create Book'
+		visit books_path
+		expect(page).to have_content('Isaac Asimov')
+	end
+end
+#=end
+
+#=begin
+#integration test for price (number)
+RSpec.describe 'Book with price', type: :feature do
+	scenario 'valid inputs' do
+		visit new_book_path
+		fill_in 'Price', with: '12.99'
+		click_on 'Create Book'
+		visit books_path
+		expect(page).to have_content('12.99')
+	end
+end
+#=end
+
+#=begin
+#integration test for published date (Date)
+RSpec.describe 'Book with published date', type: :feature do
+	scenario 'valid inputs' do
+		visit new_book_path
+		fill_in 'Published_date', with: '2000-08-15'
+		click_on 'Create Book'
+		visit books_path
+		expect(page).to have_content('2000-08-15')
+	end
+end
+#=end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -4,14 +4,20 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
 	subject do
 		described_class.new(title: 'harry potter')
-	end 
-	
+		described_class.new(author: 'Isaac Asimov')
+		described_class.new(price: '12.99')
+		described_class.new(published_date: '2000-08-15')
+	end
+
 	it 'is valid with valid attributes' do
 		expect(subject).to be_valid
 	end
-	
+
 	it 'is not valid without a name' do
-		subject.title = nil 
-		expect(subject).not_to be_valid 
-	end 
-end 
+		subject.title = nil
+		subject.author = nil
+		subject.price = nil
+		subject.published_date = nil
+		expect(subject).not_to be_valid
+	end
+end
